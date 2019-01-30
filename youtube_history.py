Remove .ix and fix average_rating.
YouTube got rid of 5 star ratings. 
Average rating is now calculated as:

like_counts/(like_counts + dislike_counts)

youtube_history.py
@@ -30,30 +30,30 @@ from grapher import Grapher
 
 
 app = Flask(__name__)
-                         
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html', analysis=analysis)
 
 def launch_web():
     app.debug = False
-    app.secret_key = 'this key should be complex'      
+    app.secret_key = 'this key should be complex'
 
     file1 = os.path.join(analysis.raw, '00001.info.json')
     some_data = os.path.isfile(file1)
-    if some_data:    
+    if some_data:
         url = 'http://127.0.0.1:5000'
         open_new_tab(url)
         app.run()
-    
+
 class Analysis():
     """Main class responsible for downloading and analyzing data.
-    
+
     Parameters
     ----------
     path : str (default='data')
         The path to the directory where both raw and computed results should be stored.
-        
+
     Attributes
     ----------
     raw : str
@@ -66,7 +66,7 @@ class Analysis():
         A list of tags for each downloaded video
     grapher : Grapher
         Creates the interactive graphs portion of the analysis
-        
+
     seconds : int
         The sum of video durations
     formatted_time : str
@@ -98,8 +98,8 @@ class Analysis():
         self.ran = os.path.join(self.path, 'ran')
         self.df = None
         self.tags = None
-        self.grapher = None  
-        
+        self.grapher = None
+
         self.seconds= None
         self.formatted_time = None
         self.all_likes = None
@@ -112,7 +112,7 @@ class Analysis():
         self.top_uploaders = None
         self.funny = None
         self.funny_counts = None
-        
+
     def download_data(self):
         """Uses youtube_dl to download individual json files for each video."""
         print('There\'s no data in this folder. Let\'s download some.')
@@ -135,10 +135,10 @@ class Analysis():
               if line == 'WARNING: unable to log in: bad username or password':
                   successful_login = False
               if not line: break
-    
+
     def df_from_files(self):
         """Constructs a Dataframe from the downloaded json files.
-        
+
         All json keys whose values are not lists are compiled into the dataframe.
         The dataframe is then saved as a csv file in the self.ran directory.
         The tags of each video are pickled and saved as tags.txt
@@ -148,7 +148,7 @@ class Analysis():
         files = os.path.join(self.raw, '~.info.json') # This is a weird hack
         files = files.replace('~', '{:05d}') # It allows path joining to work on Windows
         data = [json.load(open(files.format(i))) for i in range(1, num + 1)]
-        
+
         columns = ['formats', 'tags', 'categories', 'thumbnails']
         lists = [[], [], [], []]
         deletes = {k:v for k, v in zip(columns, lists)}
@@ -159,22 +159,22 @@ class Analysis():
 
         self.df = pd.DataFrame(data)
         self.df['upload_date'] = pd.to_datetime(self.df['upload_date'], format='%Y%m%d')
-        self.df.to_csv(os.path.join(self.ran,'df.csv'))  
+        self.df.to_csv(os.path.join(self.ran, 'df.csv'))
 
-        self.tags = deletes['tags']                
+        self.tags = deletes['tags']
         pickle.dump(self.tags, open(os.path.join(self.ran, 'tags.txt'), 'wb'))
-        
+
     def make_wordcloud(self):
         """Generate the wordcloud file and save it to static/images/."""
         #plt.rcParams['figure.figsize'] = [24.0, 18.0]
         print('Creating wordcloud')
         flat_tags = [item for sublist in self.tags for item in sublist]
-        wordcloud = WordCloud(width=1920, 
-                              height=1080, 
+        wordcloud = WordCloud(width=1920,
+                              height=1080,
                               relative_scaling=.5)
         wordcloud.generate(' '.join(flat_tags))
         wordcloud.to_file(os.path.join('static', 'images', 'wordcloud.png'))
-        
+
     def check_df(self):
         """Create the dataframe and tags from files if file doesn't exist."""
         if not os.path.exists(self.ran):
@@ -198,9 +198,9 @@ class Analysis():
             ('minutes', 60),
             ('seconds', 1)
             )
-            
+
         result = []
-    
+
         for name, count in intervals:
             value = seconds // count
             if value:
@@ -210,22 +210,23 @@ class Analysis():
                 result.append("{} {}".format(value, name))
         self.formatted_time = ', '.join(result)
 
-
     def worst_videos(self):
         """Finds the lowest rated and most disliked videos"""
-        df_liked = self.df[self.df.like_count > 0]
-        self.lowest_rating = df_liked.ix[df_liked['average_rating'].idxmin()]
-        self.most_disliked = self.df.ix[self.df['dislike_count'].idxmax()]
+        self.df['total_votes'] = self.df['like_count'] + self.df['dislike_count']
+        self.df['average_rating'] = self.df['like_count'] / self.df['total_votes']
+        df_voted = self.df[self.df['total_votes'] > 0]
+        self.lowest_rating = df_voted.loc[df_voted['average_rating'].idxmin()]
+        self.most_disliked = self.df.loc[self.df['dislike_count'].idxmax()]
 
     def best_videos(self):
         """Finds well liked and highly viewed videos"""
-        all_likes = self.df[self.df.average_rating == 5]
+        all_likes = self.df[(self.df['like_count'] > 0) & (self.df['dislike_count'] == 0)]
         all_likes = all_likes.sort_values('like_count', ascending=False)
         self.all_likes = all_likes.iloc[0]
-        
-        self.most_liked = self.df.ix[self.df['like_count'].idxmax()]
-        self.most_viewed = self.df.ix[self.df['view_count'].idxmax()]
-    
+
+        self.most_liked = self.df.loc[self.df['like_count'].idxmax()]
+        self.most_viewed = self.df.loc[self.df['view_count'].idxmax()]
+
     def funniest_description(self):
         """Counts number of times 'funny' is in each description. Saves top result."""
         funny_counts = []
@@ -238,28 +239,28 @@ class Analysis():
                 index.append(i)
             except AttributeError:
                 pass
-        funny_counts = np.array(funny_counts)  
+        funny_counts = np.array(funny_counts)
         funny_counts_idx = funny_counts.argmax()
         self.funny_counts = funny_counts[funny_counts_idx]
         if self.funny_counts > 0:
             self.funny = self.df.iloc[index[funny_counts_idx]]
         else:
             self.funny = 'Wait, 0? You\'re too cool to watch funny videos on youtube?'
-            
+
     def three_randoms(self):
         """Finds results for video resolutions, most popular channels, and funniest video."""
         self.HD = self.df[(720 <= self.df.height) & (self.df.height <= 1080)].shape[0]
         self.UHD = self.df[self.df.height > 1080].shape[0]
         self.top_uploaders = self.df.uploader.value_counts().head(n=15)
         self.funniest_description()
-        
+
     def compute(self):
         print('Computing...')
         self.total_time()
         self.worst_videos()
         self.best_videos()
         self.oldest_videos = self.df[['title', 'webpage_url']].tail(n=10)
-        self.oldest_upload = self.df.ix[self.df['upload_date'].idxmin()]
+        self.oldest_upload = self.df.loc[self.df['upload_date'].idxmin()]
         self.three_randoms()
 
     def graph(self):
@@ -268,14 +269,14 @@ class Analysis():
         self.grapher.duration()
         self.grapher.views()
         self.grapher.gen_tags_plot()
-        
+
     def start_analysis(self):
         self.check_df()
         if WordCloud is not None:
             self.make_wordcloud()
         self.compute()
         self.graph()
-        
+
     def run(self):
         """Main function for downloading and analyzing data."""
         file1 = os.path.join(self.raw, '00001.info.json')